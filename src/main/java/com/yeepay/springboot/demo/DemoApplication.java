b2d6eb55dfff16d659cc2c6b49b9cf08f4f4848f
huocheng.zhang <huocheng.zhang@yeepay.com>
2020-11-16 11:54:40 +0800

    springboot devtool

---
+++ src/main/java/com/yeepay/springboot/demo/DemoApplication.java
@@ -0,0 +1,39 @@
+package com.yeepay.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		System.out.println("=======");
+
+
+		System.out.println("test" +
+				"");
+		System.out.println("+++++++++++=");
+		System.out.println("慝身体" +
+				"" +
+				"" +
+				"");
+
+		System.out.println("");
+
+		System.out.println("test");
+		System.out.println("[[[[[[[[");
+
+		System.out.println("[[[");
+		return String.format("Hello %s!", name);
+	}
+}
